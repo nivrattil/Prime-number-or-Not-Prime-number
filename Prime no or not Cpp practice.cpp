190073f74f8fad5261a1da7906a351e1fd3aba34
Nivratti Waghmode <94159696+nivrattil@users.noreply.github.com>
2023-02-27 01:16:40 +0530

    Update Prime no or not Cpp practice.cpp

--- Prime no or not Cpp practice.cpp
+++ Prime no or not Cpp practice.cpp
@@ -1,4 +1,4 @@
-/* Check wether given number is prime number or not */
+/* Check whether given number is prime number or not */
 #include <iostream>
 using namespace std;
 int main(){
@@ -15,3 +15,47 @@ int main(){
 	else
 	cout<< "Entered number is not prime";
 }
+/* OR */
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i, num, sum;
+	cout<< "Enter number: ";
+	cin >> num;
+	for(i=2; i<num; i++)
+	{
+		if(num%i==0){
+		sum++;
+		}
+	}
+	if(sum==1){
+		cout<< "Prime Number";
+	}
+	else
+	{
+		cout<< "Not Prime Number";
+	}
+	return 0;
+}
+/* OR */
+#include <iostream>
+using namespace std;
+int main(){
+int num, i, Sum=0;
+cout<<"Enter Number:";
+cin>>num;
+for(i=2; i<num; i++){
+if(num%i==0){
+Sum++;
+}
+}
+if(Sum==0){
+cout<<"num is prime";
+}
+else{
+cout<<"num is not prime";
+}
+return 0;
+}
+
